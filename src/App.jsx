abd5683b348eaa839d69396087fcaf546d93ef68
Harshith Renikindi <harshith.renikidi@kalvium.community>
2025-02-16 15:40:14 +0530

    done the assignemnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Usercard from './components/usercard.jsx';
 
 function App() {
-  const cards = Array.from({ length: 10 });
   const gridStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, 1fr)',
@@ -15,9 +11,7 @@ function App() {
 
   return (
     <div style={gridStyle}>
-      {cards.map((_, index) => (
-        <Usercard key={index} />
-      ))}
+        <Usercard/>
     </div>
   );
 }
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
 // write the component code here
+import React from 'react';
+import profileImage from '../assets/Priyanka Arul Mohan.jpg';
+
+const Usercard=()=>{
+    const name="Harshith"
+    const email="harshithrenikindi@gmail.com"
+    const phone="9390048916"
+    const address="Nizamabad,Telangana"
+
+    return(
+        <div>
+            <h1>Name:{name}</h1>
+            <p><strong>Email:{email}</strong></p>
+            <p><strong>Phone:{phone}</strong></p>
+            <p><strong>Address:{address}</strong></p>
+            <h1>Favourite Heorine</h1>
+            <img src={profileImage} alt="image"/>
+        </div>
+    )
+}
+
+export default Usercard;
